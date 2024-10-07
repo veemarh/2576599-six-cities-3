317652503794fa07bc2701852f33921c9471154c
veta <vee.marh.my@gmail.com>
2024-10-07 17:09:30 +0300

    cities added to the map

--- src/shared/types/city.type.ts
+++ src/shared/types/city.type.ts
@@ -4,6 +4,10 @@ export type CityCoordinatesType = {
 };
 
 export const cityCoordinatesMap: Record<string, CityCoordinatesType> = {
-  'Tokyo': {latitude: 35.6762, longitude: 139.6503},
-  'Paris': {latitude: 48.8566, longitude: 2.3522},
+  'Paris': {latitude: 48.85661, longitude: 2.351499},
+  'Cologne': {latitude: 50.938361, longitude: 6.959974},
+  'Brussels': {latitude: 50.846557, longitude: 4.351697},
+  'Amsterdam': {latitude: 52.370216, longitude: 4.895168},
+  'Hamburg': {latitude: 53.550341, longitude: 10.000654},
+  'Dusseldorf': {latitude: 51.225402, longitude: 6.776314},
 };
